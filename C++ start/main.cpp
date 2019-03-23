bb6bd75a966c2ac6c944758c54954298bc144a08
Vo Phi Hung <vophihung@grooo.co.jp>
2019-03-23 23:04:22 +0700

    Initial Commit

---
+++ C++ start/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  C++ start
+//
+//  Created by Vo Phi Hung   on 2019/03/23.
+//  Copyright © 2019 Vo Phi Hung  . All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
